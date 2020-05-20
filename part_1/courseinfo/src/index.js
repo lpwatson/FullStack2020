53b8cc75c6b79db843e03e45d599ca53254bd2d4
Liam Watson <lwatson@shopkeep.com>
2020-05-20 09:46:05 +0100

    Exercises 1.2

--- part_1/courseinfo/src/index.js
+++ part_1/courseinfo/src/index.js
@@ -3,35 +3,40 @@ import ReactDOM from 'react-dom';
 
 const Header = (props) => {
   return (
-    <div>
+    <>
       <h1>{props.course}</h1>
-    </div>
+    </>
+  )
+}
+
+const Part = (props) => {
+
+  return(
+    <>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </>
   )
 }
 
 const Content = (props) => {
 
   return (
-    <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </div>
+    <>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </>
   )
 }
 
 const Total = (props) => {
 
   return (
-    <div>
+    <>
       <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </div>
+    </>
   )
 }
 
@@ -45,12 +50,12 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
+    <>
       <Header course={course} />
       <Content part1={part1} exercises1={exercises1} part2={part2}
                 exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-    </div>
+    </>
   )
 }
 
