39baf23cf27ad8b173adb04835a8c0a9decfb405
Liam Watson <lwatson@shopkeep.com>
2020-05-23 16:21:34 +0100

    exercise 1.8

--- part_1/unicafe/src/index.js
+++ part_1/unicafe/src/index.js
@@ -17,11 +17,25 @@ const Display = (props) => (
   </div>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+
+  return (
+    <div>
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />
+      <Display text="all" value={good + neutral + bad} />
+      <Display text="average" value={(good - bad) / (good + bad + neutral)} />
+      <Display text="positive" value={(good /(good + neutral + bad) * 100)} />
+    </div>
+
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
 
   return (
     <div>
@@ -30,12 +44,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Header text="statistics" />
-      <Display text="good" value={good} />
-      <Display text="neutral" value={neutral} />
-      <Display text="bad" value={bad} />
-      <Display text="all" value={good + neutral + bad} />
-      <Display text="average" value={(good - bad) / (good + bad + neutral)} />
-      <Display text="positive" value={(good /(good + neutral + bad) * 100)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
